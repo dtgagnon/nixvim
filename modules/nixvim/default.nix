c6d48baca4b4b0040437202bbf8547fb40747add
dtgagnon <gagnon.derek@gmail.com>
2024-10-28 17:03:24 -0400

    believed to be reset to function and buildable config

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -1,152 +1,114 @@
-{ lib
-, pkgs
-, config
-, ...
-}:
-let
-  inherit (lib) types mkIf mkOption;
-  cfg = config.spirenix-nvim.nixvim;
-
-  availableThemes = types.enum [
-    "aquarium"
-    "decay"
-    "edge-dark"
-    "everblush"
-    "everforest"
-    "gruvbox"
-    "jellybeans"
-    "mountain"
-    "nord"
-    "oxo-carbon"
-    "paradise"
-    "tokyonight"
-  ];
-in
+{ pkgs, ... }:
 {
-  options.spirenix-nvim.nixvim = {
-    enable = mkOption {
-      type = types.bool;
-      default = false;
-      description = "Enable custom neovim config'd with nixvim";
-    };
-    themeName = mkOption {
-      type = availableThemes;
-      default = "everforest";
-      description = "Choose the base-16 neovim theme";
-    };
+  vimAlias = true;
+  withNodeJs = true;
+  withRuby = true;
+
+  luaLoader.enable = true;
+
+  extraPlugins = with pkgs.vimPlugins; [ aider-nvim vim-bufkill ];
+
+  globals = {
+    mapleader = " ";
+    maplocalleader = " ";
   };
 
-  config = mkIf cfg.enable {
-    vimAlias = true;
-    withNodeJs = true;
-    withRuby = true;
+  opts = {
+    # smartindent = true;
+    breakindent = true;
+    cursorline = true;
 
-    luaLoader.enable = true;
+    tabstop = 2;
+    shiftwidth = 2;
 
-    extraPlugins = with pkgs.vimPlugins; [ aider-nvim vim-bufkill ];
+    wrap = true;
+    linebreak = true;
 
-    globals = {
-      mapleader = " ";
-      maplocalleader = " ";
-    };
+    number = true;
+    relativenumber = true;
 
-    opts = {
-      # smartindent = true;
-      breakindent = true;
-      cursorline = true;
+    signcolumn = "yes:1";
 
-      tabstop = 2;
-      shiftwidth = 2;
+    mouse = "a";
 
-      wrap = true;
-      linebreak = true;
+    foldcolumn = "0";
+    foldlevel = 99;
+    foldlevelstart = 99;
+    foldenable = true;
 
-      number = true;
-      relativenumber = true;
+    updatetime = 50;
 
-      signcolumn = "yes:1";
+    swapfile = false;
+    backup = false;
+    undofile = true;
 
-      mouse = "a";
+    termguicolors = true;
 
-      foldcolumn = "0";
-      foldlevel = 99;
-      foldlevelstart = 99;
-      foldenable = true;
+    ignorecase = true;
+    smartcase = true;
 
-      updatetime = 50;
+    splitbelow = true;
+    splitright = true;
 
-      swapfile = false;
-      backup = false;
-      undofile = true;
+    scrolloff = 4;
 
-      termguicolors = true;
-
-      ignorecase = true;
-      smartcase = true;
-
-      splitbelow = true;
-      splitright = true;
-
-      scrolloff = 4;
-
-      laststatus = 3;
-    };
-
-    keymaps = [
-      {
-        mode = "n";
-        key = "<leader>ch";
-        action = "<cmd>noh<cr>";
-        options = {
-          silent = true;
-          desc = "Clear highlight";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>cs";
-        action = ''<cmd>let @/=""<cr>'';
-        options = {
-          silent = true;
-          desc = "Clear search";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>bd";
-        action = "<cmd>:BD<cr>";
-        options = {
-          silent = true;
-          desc = "Delete buffer";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>bn";
-        action = "<cmd>:bnext<cr>";
-        options = {
-          silent = true;
-          desc = "Next buffer";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>bp";
-        action = "<cmd>:bprevious<cr>";
-        options = {
-          silent = true;
-          desc = "Previous buffer";
-        };
-      }
-      {
-        mode = "t";
-        key = "<C-o>";
-        action = "<C-\\><C-n>";
-        options = {
-          silent = true;
-          desc = "Exit terminal mode";
-        };
-      }
-    ];
+    laststatus = 3;
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>ch";
+      action = "<cmd>noh<cr>";
+      options = {
+        silent = true;
+        desc = "Clear highlight";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>cs";
+      action = ''<cmd>let @/=""<cr>'';
+      options = {
+        silent = true;
+        desc = "Clear search";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>bd";
+      action = "<cmd>:BD<cr>";
+      options = {
+        silent = true;
+        desc = "Delete buffer";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>bn";
+      action = "<cmd>:bnext<cr>";
+      options = {
+        silent = true;
+        desc = "Next buffer";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>bp";
+      action = "<cmd>:bprevious<cr>";
+      options = {
+        silent = true;
+        desc = "Previous buffer";
+      };
+    }
+    {
+      mode = "t";
+      key = "<C-o>";
+      action = "<C-\\><C-n>";
+      options = {
+        silent = true;
+        desc = "Exit terminal mode";
+      };
+    }
+  ];
 }
