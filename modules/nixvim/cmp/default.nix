3ab4e4385aedb63e8c3519697c0a49e777ff7002
dtgagnon <gagnon.derek@gmail.com>
2024-12-01 17:02:32 -0500

    removed copilot from cmp module

--- modules/nixvim/cmp/default.nix
+++ modules/nixvim/cmp/default.nix
@@ -1,4 +1,5 @@
-{ helpers, ... }: {
+{ helpers, ... }:
+{
   plugins = {
     luasnip.enable = true;
 
@@ -8,64 +9,67 @@
       cmdline =
         let
           search = {
-            mapping = helpers.mkRaw # lua
-              "cmp.mapping.preset.cmdline()";
-            sources = [{ name = "buffer"; }];
+            mapping =
+              helpers.mkRaw # lua
+                "cmp.mapping.preset.cmdline()";
+            sources = [ { name = "buffer"; } ];
           };
         in
         {
           "/" = search;
           "?" = search;
           ":" = {
-            mapping = helpers.mkRaw # lua
-              "cmp.mapping.preset.cmdline()";
-            sources = [{ name = "cmdline"; }];
+            mapping =
+              helpers.mkRaw # lua
+                "cmp.mapping.preset.cmdline()";
+            sources = [ { name = "cmdline"; } ];
           };
         };
 
       settings = {
-        experimental = { ghost_text = true; };
+        experimental = {
+          ghost_text = true;
+        };
 
         mapping = {
           "<C-Space>" = "cmp.mapping.complete()";
           "<C-j>" = "cmp.mapping.scroll_docs(4)";
           "<C-k>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-l>" =
-            "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
-          "<C-n>" = helpers.mkRaw # lua
-            ''
-              function(fallback)
-                if cmp.visible() then
-                  cmp.select_next_item()
-                elseif require("luasnip").expand_or_jumpable() then
-                  require("luasnip").expand_or_jump()
-                else
-                  fallback()
+          "<C-l>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+          "<C-n>" =
+            helpers.mkRaw # lua
+              ''
+                function(fallback)
+                  if cmp.visible() then
+                    cmp.select_next_item()
+                  elseif require("luasnip").expand_or_jumpable() then
+                    require("luasnip").expand_or_jump()
+                  else
+                    fallback()
+                  end
                 end
-              end
-            '';
-          "<C-p>" = helpers.mkRaw # lua
-            ''
-              function(fallback)
-                if cmp.visible() then
-                  cmp.select_prev_item()
-                elseif require("luasnip").expand_or_jumpable() then
-                  require("luasnip").expand_or_jump()
-                else
-                  fallback()
+              '';
+          "<C-p>" =
+            helpers.mkRaw # lua
+              ''
+                function(fallback)
+                  if cmp.visible() then
+                    cmp.select_prev_item()
+                  elseif require("luasnip").expand_or_jumpable() then
+                    require("luasnip").expand_or_jump()
+                  else
+                    fallback()
+                  end
                 end
-              end
-            '';
+              '';
         };
 
         snippet = {
-          expand =
-            ''function(args) require("luasnip").lsp_expand(args.body) end'';
+          expand = ''function(args) require("luasnip").lsp_expand(args.body) end'';
         };
 
         sources = [
           { name = "path"; }
-          { name = "copilot"; }
           { name = "nvim_lsp"; }
           { name = "luasnip"; }
           { name = "buffer"; }
@@ -75,7 +79,6 @@
     };
 
     # cmp-cmdline.enable = true;
-    # copilot-cmp.enable = true;
     # cmp-dictionary.enable = true;
     # cmp-fuzzy-path.enable = true;
     # cmp-fuzzy-buffer.enable = true;
