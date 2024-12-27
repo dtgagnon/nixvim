ff86f7c5b743c42c580e4bdc2bcf19b0cd4ad68b
dtgagnon <gagnon.derek@gmail.com>
2024-12-27 14:51:11 -0500

    trying to get theme into custom neovim

--- flake.nix
+++ flake.nix
@@ -2,24 +2,24 @@
   description = "A custom neovim configuration using nixvim.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     nixvim.url = "github:nix-community/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
 
     snowfall-lib.url = "github:snowfallorg/lib";
-    snowfall-lib.inputs.nixpkgs.follows = "stable";
+    snowfall-lib.inputs.nixpkgs.follows = "nixpkgs";
 
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-    pre-commit-hooks.inputs.nixpkgs.follows = "stable";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   # TODO: a todo
   # NOTE: a note
   # WARN: a warning
   # HACK: a hack
-  outputs = inputs:
+  outputs =
+    inputs:
     inputs.snowfall-lib.mkFlake {
       inherit inputs;
       src = ./.;
@@ -34,17 +34,16 @@
 
       outputs-builder = channels: {
         formatter = channels.nixpkgs.nixfmt-rfc-style;
-        checks.pre-commit-check =
-          inputs.pre-commit-hooks.lib.${channels.nixpkgs.system}.run {
-            src = ./.;
-            hooks = {
-              nixfmt-rfc-style = {
-                enable = true;
-                entry = "${channels.nixpkgs.nixfmt-rfc-style}/bin/nixfmt";
-                extraPackages = [ channels.nixpkgs.hooks.nixfmt-rfc-style ];
-              };
+        checks.pre-commit-check = inputs.pre-commit-hooks.lib.${channels.nixpkgs.system}.run {
+          src = ./.;
+          hooks = {
+            nixfmt-rfc-style = {
+              enable = true;
+              entry = "${channels.nixpkgs.nixfmt-rfc-style}/bin/nixfmt";
+              extraPackages = [ channels.nixpkgs.hooks.nixfmt-rfc-style ];
             };
           };
+        };
       };
     };
 }
