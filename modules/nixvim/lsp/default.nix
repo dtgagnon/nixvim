92ad5012730525dae3dc4849e69e5e1dc61a2351
dtgagnon <gagnon.derek@gmail.com>
2025-07-07 14:06:16 -0400

    minor fix

--- modules/nixvim/lsp/default.nix
+++ modules/nixvim/lsp/default.nix
@@ -19,18 +19,22 @@ in
 
   extraConfigLuaPre = ''
     do
-      local diagnostic_signs = { Error = "", Warn = "", Hint = "", Info = "" }
-
-      for type, icon in pairs(diagnostic_signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-      end
-
-      vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+      local diagnostic_signs = { Error = "", Warn = "", Hint = "", Info = "" }
+      
+      -- Modern way to configure diagnostic signs
+      vim.diagnostic.config({
         underline = true,
         update_in_insert = true,
         virtual_text = { spacing = 4, prefix = "●" },
         severity_sort = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = diagnostic_signs.Error,
+            [vim.diagnostic.severity.WARN] = diagnostic_signs.Warn,
+            [vim.diagnostic.severity.HINT] = diagnostic_signs.Hint,
+            [vim.diagnostic.severity.INFO] = diagnostic_signs.Info,
+          }
+        }
       })
     end
   '';
