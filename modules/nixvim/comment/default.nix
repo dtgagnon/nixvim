bb7c9ef85b6b61dce5ac4127c4d24ece61389865
dtgagnon <gagnon.derek@gmail.com>
2025-07-07 12:58:39 -0400

    minor fix

--- modules/nixvim/comment/default.nix
+++ modules/nixvim/comment/default.nix
@@ -2,21 +2,20 @@
 {
   plugins.comment.enable = true;
 
-  extraConfigLua = helpers.mkRaw # lua
-    ''
-      require("which-key").register({
-        -- Normal Mode
-        { "g", group = "Comment", mode = "n" },
-        { "gc", "<cmd>CommentToggleCurrentLine<cr>", desc = "Toggle Comment (Line)", mode = "n" },
-        { "gb", "<cmd>CommentToggleCurrentBlock<cr>", desc = "Toggle Comment (Block)", mode = "n" },
-        { "go", "<cmd>CommentInsertBelow<cr>", desc = "Comment Insert Below", mode = "n" },
-        { "gO", "<cmd>CommentInsertAbove<cr>", desc = "Comment Insert Above", mode = "n" },
-        { "gA", "<cmd>CommentInsertEndOfLine<cr>", desc = "Comment Insert End of Line", mode = "n" },
+  extraConfigLua = ''
+    require("which-key").register({
+      -- Normal Mode
+      { "g", group = "Comment", mode = "n" },
+      { "gc", "<cmd>CommentToggleCurrentLine<cr>", desc = "Toggle Comment (Line)", mode = "n" },
+      { "gb", "<cmd>CommentToggleCurrentBlock<cr>", desc = "Toggle Comment (Block)", mode = "n" },
+      { "go", "<cmd>CommentInsertBelow<cr>", desc = "Comment Insert Below", mode = "n" },
+      { "gO", "<cmd>CommentInsertAbove<cr>", desc = "Comment Insert Above", mode = "n" },
+      { "gA", "<cmd>CommentInsertEndOfLine<cr>", desc = "Comment Insert End of Line", mode = "n" },
 
-        -- Visual Mode
-        { "g", group = "Comment", mode = "v" },
-        { "gc", "<cmd>CommentToggle<cr>", desc = "Toggle Comment (Selection)", mode = "v" },
-        { "gb", "<cmd>CommentToggle<cr>", desc = "Toggle Comment (Selection)", mode = "v" }
-      })
-    '';
+      -- Visual Mode
+      { "g", group = "Comment", mode = "v" },
+      { "gc", "<cmd>CommentToggle<cr>", desc = "Toggle Comment (Selection)", mode = "v" },
+      { "gb", "<cmd>CommentToggle<cr>", desc = "Toggle Comment (Selection)", mode = "v" }
+    })
+  '';
 }
